f358840d3eab1abb5d415e28505467788b0b9721
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-02-21 14:45:06 -0500

    mod updates

--- startserver.bat
+++ startserver.bat
@@ -1,1 +1,1 @@
-java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.1.2607-universal.jar nogui
+java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.2.2618-universal.jar nogui
