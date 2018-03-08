2d0b0fa202c681f75fc59f8b18c4840020162587
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-03-08 15:33:09 -0500

    updates + add gravestone mod

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.2.2618-universal.jar nogui
+java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.2.2624-universal.jar nogui
