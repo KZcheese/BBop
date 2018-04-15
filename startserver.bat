6664941f3ec8c7acd6bfac8ef1a2fc5b7c26318a
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-04-15 00:02:11 -0400

    lots of updates + immersive engineering and draconic evolution w/ dependencies

--- startserver.bat
+++ startserver.bat
@@ -1,1 +1,1 @@
-java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.2.2624-universal.jar nogui
+java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.3.2666-universal.jar nogui
