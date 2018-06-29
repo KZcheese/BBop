211b5356b25be25c94b4f7b70692d3d71e02c38c
Kevin Zhan <kevizha@amazon.com>
2018-06-29 15:40:39 -0700

    partial mod updates, need to complete forge install

---
+++ runserver.bat
@@ -0,0 +1,1 @@
+java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.3.2680-universal.jar nogui
--- startserver.bat
+++
@@ -1,1 +0,0 @@
-java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.3.2680-universal.jar nogui
