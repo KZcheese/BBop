4550e63d72ed24e8d09c3f851a0fd5f0f4c500af
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-06-29 18:09:36 -0400

    let's see if I can get away with updating the mods at work

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.3.2680-universal.jar nogui
+java -Xmx4096M -Xms1024M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.3.2680-universal.jar nogui
