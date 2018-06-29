62d696b7fd432da0309ed41b68c540cb4f233d3d
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-06-29 18:51:49 -0400

    complete forge upgrade, add .zip files to gitignore to prevent failure due to massive backup files

--- runserver.bat
+++ runserver.bat
@@ -1,1 +1,1 @@
-java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.3.2680-universal.jar nogui
+java -Xmx2048M -Xms2048M -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy -d64 -jar forge-1.12.2-14.23.4.2725-universal.jar nogui
